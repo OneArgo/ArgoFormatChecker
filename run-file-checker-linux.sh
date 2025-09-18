201d6466f7c4a4d1363f449a86745c2cc5b7762f
BRUVRY-LAGADEC <leo.brurvy.lagadec@ifremer.fr>
2025-09-18 19:13:21 +0200

    feat: demonstration
    Improve Linux demo process

--- run-file-checker-linux.sh
+++ run-file-checker-linux.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# TODO : add when container can run as non root
-# export DOCKER_UID=$UID
-# export DOCKER_GID=$(id -g $UID)
+export DOCKER_UID=$UID
+export DOCKER_GID=$(id -g $UID)
 
+# BODC file format checker
+echo "----- File format checker for BODC 3901945 -----"
 docker compose --env-file .env.demo.bodc down
 docker compose --env-file .env.demo.bodc up
+
+# Coriolis file format checker
+echo "----- File format checker for COriolis 2903996 -----"
+docker compose --env-file .env.demo.coriolis down
+docker compose --env-file .env.demo.coriolis up
